8916b1b1fc8c1d09051a2ba9e8ae2ae382c7e73a
acuodancer <leminhduc74@gmail.com>
2016-09-29 13:22:15 +0800

    Add ShowFavoriteCommand extending Command to show all favorited contacts (copy FindCommand code and change name word matching to check getIsFavorite == true)

---
+++ src/seedu/addressbook/commands/ShowFavoriteCommand.java
@@ -0,0 +1,41 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ShowFavoriteCommand extends Command {
+
+    public static final String COMMAND_WORD = "showfav";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+            + "Displays all persons in the favorite list as a list with index numbers.\n\t"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allFavoriteContacts = getPersonsInFavorite();
+        return new CommandResult(getMessageForPersonListShownSummary(allFavoriteContacts), allFavoriteContacts);
+    }
+    
+    private List<ReadOnlyPerson> getPersonsInFavorite() {
+        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        for (ReadOnlyPerson roPerson : addressBook.getAllPersons()) {
+        	Person person = new Person(roPerson);
+            if (person.getIsFavorite() == true) {
+            	matchedPersons.add(roPerson);
+            }
+        }
+        return matchedPersons;
+    }
+}
