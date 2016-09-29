d9aa8d2bf239b9fed5d4aea6fbe8b15e0e680d60
acuodancer <leminhduc74@gmail.com>
2016-09-29 13:17:44 +0800

    Remove unused imports from FavoriteCommand

--- src/seedu/addressbook/commands/FavoriteCommand.java
+++ src/seedu/addressbook/commands/FavoriteCommand.java
@@ -1,11 +1,5 @@
 package seedu.addressbook.commands;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
